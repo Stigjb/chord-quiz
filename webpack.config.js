9c71e424ea644231bfc43e96fd5b3aba4b482930
Stig Johan Berggren <stigjb@gmail.com>
2020-05-15 13:58:52 +0200

    Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,36 @@
-const path = require('path');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const distPath = path.resolve(__dirname, "dist");
 module.exports = (env, argv) => {
   return {
     devServer: {
       contentBase: distPath,
-      compress: argv.mode === 'production',
-      port: 8000
+      compress: argv.mode === "production",
+      port: 8000,
     },
-    entry: './bootstrap.js',
+    entry: "./bootstrap.js",
     output: {
       path: distPath,
       filename: "chord_quiz.js",
-      webassemblyModuleFilename: "chord_quiz.wasm"
+      webassemblyModuleFilename: "chord_quiz.wasm",
     },
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
       ],
     },
     plugins: [
-      new CopyWebpackPlugin([
-        { from: './static', to: distPath }
-      ]),
+      new CopyWebpackPlugin([{ from: "./static", to: distPath }]),
       new WasmPackPlugin({
         crateDirectory: ".",
         extraArgs: "--no-typescript",
-      })
+      }),
     ],
-    watch: argv.mode !== 'production'
+    watch: argv.mode !== "production",
   };
 };
